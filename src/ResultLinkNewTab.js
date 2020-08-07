780f33ece67979becef79e4b3b4936887c3165e5
Jean-Francois Allaire <jfallaire@coveo.com>
2020-08-07 14:23:50 -0400

    Add support for mulitple search interfaces in same page

--- src/ResultLinkNewTab.js
+++ src/ResultLinkNewTab.js
@@ -1,14 +1,15 @@
 const Coveo = require('coveo-search-ui');
 
 module.exports = function ResultLinkNewTab(searchInterface) {
-    searchInterface = searchInterface || document.querySelector('.CoveoSearchInterface');
-    searchInterface.addEventListener(Coveo.InitializationEvents.beforeInitialization, function (e, args) {
-        const root = document.querySelector("#search");
-    
-        Coveo.options(root, {
-            ResultLink: {
-                alwaysOpenInNewWindow: true
-            }
+    const searchInterfaces = searchInterface ? [searchInterface] : document.querySelectorAll('.CoveoSearchInterface');
+    searchInterfaces.forEach(searchInterface => {
+        searchInterface.addEventListener(Coveo.InitializationEvents.beforeInitialization, function (e, args) {   
+            Coveo.options(e.target, {
+                ResultLink: {
+                    alwaysOpenInNewWindow: true
+                }
+            });
         });
     });
+    
 }
